02114d889c0107d3bb97bccb6cc8266d8274010d
Phat <6652@ait.nsw.edu.au>
2020-02-07 21:16:56 +1100

    Phat's changes

--- gallery.php
+++ gallery.php
@@ -1,20 +1,34 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "miladPainting");
+if ($conn) {
+    echo "connected";  
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <title>About Us - Milad Painting Website</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <style>
+  /* Make the image fully responsive */
+  .carousel-inner img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+  }
+
+  </style>
     <link rel="stylesheet" href="about.css">
     <link rel="stylesheet" href="banner.css">
     <link rel="stylesheet" href="nav.css">
     <link rel="stylesheet" href="footer.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-    <link href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" rel="stylesheet">
-
 </head>
 
 <body>
@@ -91,35 +105,52 @@
             </div>
         </div>
     </nav>
-    
-    <div id="demo" class="carousel slide" data-ride="carousel">
+    <div id="demo" class="carousel slide" data-ride="carousel" style= "width:100%; height: 700 !important; padding-bottom: 20px; background-color: grey;">
       <ul class="carousel-indicators">
-        <li data-target="#demo" data-slide-to="0" class="active"></li>
-        <li data-target="#demo" data-slide-to="1"></li>
-        <li data-target="#demo" data-slide-to="2"></li>
+        <?php 
+          $result = mysqli_query($conn, "SELECT * FROM `uploadImage`");
+          $rowCount = mysqli_num_rows ( $result );
+          for ($i=0 ; $i <= $rowCount; $i++){
+              if ($i == 0) {
+                  echo '<li data-target="#demo" data-slide-to='.$i . 'class="active"></li>';
+              }
+              else {
+                  echo '<li data-target="#demo" data-slide-to=' .$i.'></li>';
+              }             
+          }
+          ?>
       </ul>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="img/Client%2520Picture/1.jpg" alt="Los Angeles" width="1100" height="500">
-          <div class="carousel-caption">
-            <h3>Los Angeles</h3>
-            <p>We had such a great time in LA!</p>
-          </div>   
-        </div>
-        <div class="carousel-item">
-          <img src="img/Client%2520Picture/13.jpg" alt="Chicago" width="1100" height="500">
-          <div class="carousel-caption">
-            <h3>Chicago</h3>
-            <p>Thank you, Chicago!</p>
-          </div>   
-        </div>
-        <div class="carousel-item">
-          <img src="img/Client%2520Picture/23.jpg" alt="New York" width="1100" height="500">
-          <div class="carousel-caption">
-            <h3>New York</h3>
-            <p>We love the Big Apple!</p>
-          </div>   
-        </div>
+      <div class="carousel-inner" role="listbox"style="width: 100%; height: 100% !important;">
+          <?php 
+          $count =0;
+            while ($row = mysqli_fetch_array($result)) {
+                if ($count == 0){
+                    echo '<div class="carousel-item active">';
+                    echo '<br>';
+                    echo '<img src="data:image/jpeg;base64,'.base64_encode($row['imageFile']).'" alt="'.$row['LocationName'].'" height: "500" width:"1000">';
+                    echo '<br>';
+                    echo '<div class="carousel-caption" style ="color: black; font-family: "Zhi Mang Xing", cursive;" >';
+                    echo '<br>';
+                    echo '<h1>'. $row['LocationName'] .'</h1>';
+                    echo '</div>';
+                    echo '<br>';
+                    echo '</div>';
+                }
+                else {
+                    echo '<div class="carousel-item">';
+                    echo '<br>';
+                    echo '<img src="data:image/jpeg;base64,'.base64_encode($row['imageFile']).'" alt="'.$row['LocationName'].'" height: "500" width:"1000">';
+                    echo '<br>';
+                    echo '<div class="carousel-caption" style ="color: black; font-family: "Zhi Mang Xing", cursive;" ">';
+                    echo '<br>';
+                    echo '<h1>'. $row['LocationName'] .'</h1>';
+                    echo '</div>';
+                    echo '<br>';
+                    echo '</div>';
+                }
+                $count = $count +1;
+            }
+          ?>      
       </div>
       <a class="carousel-control-prev" href="#demo" data-slide="prev">
         <span class="carousel-control-prev-icon"></span>
@@ -128,8 +159,7 @@
         <span class="carousel-control-next-icon"></span>
       </a>
     </div>
-    
-
+ 
     <!-- Footer -->
     <footer class="pt-5 pb-4" id="contact">
         <div class="container">
